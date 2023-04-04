22d174b95aa59e67cee16957072472b17c7da639
DESKTOP-FH6G6K1\junch <juncho12011201@gmail.com>
2023-04-04 11:08:12 +0900

    [ADD] Class ChooseMenu 기능 추가
    메뉴번호를 입력받고 형식에 맞지 않는 입력인 경우 WrongInput 클래스 호출, 형식에 맞는 입력인 경우 모양에 대한 정보인 SelectShape 클래스 호출

--- 과제1_별찍기/과제1_별찍기/Program.cs
+++ 과제1_별찍기/과제1_별찍기/Program.cs
@@ -2,6 +2,25 @@
 
 namespace Starpoint
 {
+    class ChooseMenu
+    {
+
+        public static void InputMenuNumber()
+        {
+            Console.Write("Menu : 번호를 입력하세요: ");
+            string number = Console.ReadLine();
+            Console.Clear();
+            if ((int.Parse(number) != 1) && (int.Parse(number) != 2) && (int.Parse(number) != 3) && (int.Parse(number) != 4))
+            {
+                WrongInput.WrongMemuNumber();
+            }
+            SelectShape.TypeOfShape(int.Parse(number));
+
+        }
+
+    }
+
+
     class Program
     {
         public static void Main()

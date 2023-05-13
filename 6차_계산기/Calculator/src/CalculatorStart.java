ad0c3b2623f67ca112bf9bf9b9e4cf9014a0c6c3
조상준 <>
2023-05-13 16:20:00 +0900

    [ADD] 계산기 프레임 설정 완료

---
+++ 6차_계산기/Calculator/src/CalculatorStart.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CalculatorStart extends JFrame {
+    public CalculatorStart(){
+        setTitle("계산기");
+        setSize(322,534);
+        setLayout(new GridLayout(3,3));
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+
+
+}

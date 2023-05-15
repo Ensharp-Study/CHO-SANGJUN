ac142a3dc8cbf509ab9310f3321529583fd56cbe
조상준 <>
2023-05-15 11:40:18 +0900

    [REFACTOR] CalculatorStart클래스 리펙토링

--- 6차_계산기/Calculator/src/Controller/CalculatorStart.java
+++ 6차_계산기/Calculator/src/Controller/CalculatorStart.java
@@ -2,10 +2,12 @@ package Controller;
 
 import View.CalculatorFrame;
 
-public class CalculatorStart {
-
-    public void startCalculator(){
-        new CalculatorFrame(); // 프래임 생성
-
+    public class CalculatorStart {
+        public void startCalculator() {
+            new CalculatorFrame(); // 프래임 생성
+        }
     }
-}
+
+
+
+
